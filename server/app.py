2e9a4df2018d4f773d64225235bf368f2974a5fb
Daniel <dani3lkamanthi@gmail.com>
2024-06-24 13:50:09 +0300

    Adjusts error message

--- server/app.py
+++ server/app.py
@@ -35,7 +35,7 @@ def math(num1, operation, num2):
     elif operation == '**':
         return str(num1 ** num2)
     else:
-        return 'Invalid Operation, use: +, -, *, div or %'
+        return 'Invalid Operation, use: +, -, *, % or div'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
